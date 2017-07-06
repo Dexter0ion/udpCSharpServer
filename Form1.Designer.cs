83aace033de894a7da39e88261026a4efe5d2efa
邵博超DEXION <dexter0ion@gmail.com>
2017-07-06 16:50:23 +0800

    UDP文件传输系统-文字传输核心代码编写-初期GUI编写
    Server端(初次运用多线程)

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace udpCSharpServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnServerListen = new System.Windows.Forms.Button();
+            this.tbxServerIp = new System.Windows.Forms.TextBox();
+            this.lblServerIp = new System.Windows.Forms.Label();
+            this.lblServerPort = new System.Windows.Forms.Label();
+            this.tbxServerPort = new System.Windows.Forms.TextBox();
+            this.tbxClientPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblClientPort = new System.Windows.Forms.Label();
+            this.tbxClientIp = new System.Windows.Forms.TextBox();
+            this.btnSaveInfo = new System.Windows.Forms.Button();
+            this.tbxServerListenMsg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnServerListen
+            // 
+            this.btnServerListen.Location = new System.Drawing.Point(33, 374);
+            this.btnServerListen.Name = "btnServerListen";
+            this.btnServerListen.Size = new System.Drawing.Size(232, 83);
+            this.btnServerListen.TabIndex = 0;
+            this.btnServerListen.Text = "Server开始监听";
+            this.btnServerListen.UseVisualStyleBackColor = true;
+            this.btnServerListen.Click += new System.EventHandler(this.btnConnecTest_Click);
+            // 
+            // tbxServerIp
+            // 
+            this.tbxServerIp.Location = new System.Drawing.Point(33, 54);
+            this.tbxServerIp.Name = "tbxServerIp";
+            this.tbxServerIp.Size = new System.Drawing.Size(232, 25);
+            this.tbxServerIp.TabIndex = 1;
+            this.tbxServerIp.Text = "127.0.0.1";
+            // 
+            // lblServerIp
+            // 
+            this.lblServerIp.AutoSize = true;
+            this.lblServerIp.Location = new System.Drawing.Point(33, 33);
+            this.lblServerIp.Name = "lblServerIp";
+            this.lblServerIp.Size = new System.Drawing.Size(86, 15);
+            this.lblServerIp.TabIndex = 2;
+            this.lblServerIp.Text = "Server端IP";
+            // 
+            // lblServerPort
+            // 
+            this.lblServerPort.AutoSize = true;
+            this.lblServerPort.Location = new System.Drawing.Point(33, 92);
+            this.lblServerPort.Name = "lblServerPort";
+            this.lblServerPort.Size = new System.Drawing.Size(102, 15);
+            this.lblServerPort.TabIndex = 3;
+            this.lblServerPort.Text = "Server端Port";
+            // 
+            // tbxServerPort
+            // 
+            this.tbxServerPort.Location = new System.Drawing.Point(33, 119);
+            this.tbxServerPort.Name = "tbxServerPort";
+            this.tbxServerPort.Size = new System.Drawing.Size(232, 25);
+            this.tbxServerPort.TabIndex = 4;
+            this.tbxServerPort.Text = "11000";
+            // 
+            // tbxClientPort
+            // 
+            this.tbxClientPort.Location = new System.Drawing.Point(36, 240);
+            this.tbxClientPort.Name = "tbxClientPort";
+            this.tbxClientPort.Size = new System.Drawing.Size(232, 25);
+            this.tbxClientPort.TabIndex = 8;
+            this.tbxClientPort.Text = "11001";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "本客户端Port";
+            // 
+            // lblClientPort
+            // 
+            this.lblClientPort.AutoSize = true;
+            this.lblClientPort.Location = new System.Drawing.Point(36, 162);
+            this.lblClientPort.Name = "lblClientPort";
+            this.lblClientPort.Size = new System.Drawing.Size(83, 15);
+            this.lblClientPort.TabIndex = 6;
+            this.lblClientPort.Text = "本客户端IP";
+            // 
+            // tbxClientIp
+            // 
+            this.tbxClientIp.Location = new System.Drawing.Point(33, 180);
+            this.tbxClientIp.Name = "tbxClientIp";
+            this.tbxClientIp.Size = new System.Drawing.Size(232, 25);
+            this.tbxClientIp.TabIndex = 5;
+            this.tbxClientIp.Text = "127.0.0.1";
+            // 
+            // btnSaveInfo
+            // 
+            this.btnSaveInfo.Location = new System.Drawing.Point(33, 281);
+            this.btnSaveInfo.Name = "btnSaveInfo";
+            this.btnSaveInfo.Size = new System.Drawing.Size(232, 74);
+            this.btnSaveInfo.TabIndex = 9;
+            this.btnSaveInfo.Text = "保存连接信息";
+            this.btnSaveInfo.UseVisualStyleBackColor = true;
+            this.btnSaveInfo.Click += new System.EventHandler(this.btnSaveInfo_Click);
+            // 
+            // tbxServerListenMsg
+            // 
+            this.tbxServerListenMsg.BackColor = System.Drawing.SystemColors.Window;
+            this.tbxServerListenMsg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxServerListenMsg.Location = new System.Drawing.Point(286, 54);
+            this.tbxServerListenMsg.Multiline = true;
+            this.tbxServerListenMsg.Name = "tbxServerListenMsg";
+            this.tbxServerListenMsg.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tbxServerListenMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxServerListenMsg.Size = new System.Drawing.Size(444, 333);
+            this.tbxServerListenMsg.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 492);
+            this.Controls.Add(this.tbxServerListenMsg);
+            this.Controls.Add(this.btnSaveInfo);
+            this.Controls.Add(this.tbxClientPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblClientPort);
+            this.Controls.Add(this.tbxClientIp);
+            this.Controls.Add(this.tbxServerPort);
+            this.Controls.Add(this.lblServerPort);
+            this.Controls.Add(this.lblServerIp);
+            this.Controls.Add(this.tbxServerIp);
+            this.Controls.Add(this.btnServerListen);
+            this.Name = "Form1";
+            this.Text = "UDP-Server端-邵博超";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnServerListen;
+        private System.Windows.Forms.TextBox tbxServerIp;
+        private System.Windows.Forms.Label lblServerIp;
+        private System.Windows.Forms.Label lblServerPort;
+        private System.Windows.Forms.TextBox tbxServerPort;
+        private System.Windows.Forms.TextBox tbxClientPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblClientPort;
+        private System.Windows.Forms.TextBox tbxClientIp;
+        private System.Windows.Forms.Button btnSaveInfo;
+        private System.Windows.Forms.TextBox tbxServerListenMsg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+namespace udpCSharpServer
+{
+    public partial class Form1 : Form
+    {
+        string serverIp = null;
+        int serverPort = 0;
+        string clientIp = null;
+        int clientPort = 0;
+        UdpClient client = null;
+        string receiveString = null;
+        byte[] receiveData = null;
+        //实例化一个远程端点，IP和端口可以随意指定，等调用client.Receive(ref remotePoint)时会将该端点改成真正发送端端点 
+        IPEndPoint remotePoint = new IPEndPoint(IPAddress.Any, 0);
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnConnecTest_Click(object sender, EventArgs e)
+        {
+
+            Thread t = new Thread(listenClientMsg);
+            t.Start();
+
+        }
+
+        private void tbxServerInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSaveInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void listenClientMsg()
+        {
+            string serverIp = tbxServerIp.Text;
+            int serverPort = int.Parse(tbxServerPort.Text);
+            string clientIp = tbxClientIp.Text;
+            int clientPort = int.Parse(tbxClientPort.Text);
+            while (true)
+            {
+                client = new UdpClient(serverPort);
+                receiveData = client.Receive(ref remotePoint);//接收数据 
+                receiveString = Encoding.Default.GetString(receiveData);
+                string msgListen = receiveString;
+                tbxServerListenMsg.Text += msgListen+ "\r\n";
+                Console.Write(receiveString);
+                client.Close();//关闭连接 
+            }
+        }
+    }
+}
